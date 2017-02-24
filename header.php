e8fe0cc42943337333bb310f0b7ad2439c5e5216
SigurdMW <sigurdmwahl@gmail.com>
2017-02-24 21:13:31 +0100

    added analytics script

--- header.php
+++ header.php
@@ -22,6 +22,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92278226-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <header class="main-header">
 	<nav class="menu group">
 		<div class="container menu__container">
